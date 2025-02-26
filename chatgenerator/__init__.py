7df9b2bfd13a894b6fd88c93e2d09879c23575ec
matt <mattkjames7@gmail.com>
2025-02-26 22:52:24 +0000

    added combine chats

--- chatgenerator/__init__.py
+++ chatgenerator/__init__.py
@@ -2,3 +2,5 @@ from . import common
 from .generateMessages import generateMessages
 from .generateChats import generateChats
 from .collectChats import collectChats
+from .filterChats import filterChats
+from .combineChats import combineChats
---
+++ chatgenerator/combineChats.py
@@ -0,0 +1,28 @@
+from .common import chatgenpath
+from .collectChats import collectChats
+from .filterChats import filterChats
+import json
+
+def combineChats(filter=True,n=2):
+    """
+    Combines and optionally filters all of the chats and saves them to a single file.
+
+    Inputs
+    ======
+    filter :  bool
+        If True, some of the chats will be filtered out
+    n : int
+        Number of messages to examine while filtering
+
+    """
+
+    chats = collectChats()
+    if filter:
+        chats = filterChats(chats,n)
+    
+    chatFile = f"{chatgenpath}/chats.json"
+    with open(chatFile,"w") as f:
+        json.dump(chats,f,indent=2)
+    
+    print(f"Combined chats saved in: {chatFile}")
+
